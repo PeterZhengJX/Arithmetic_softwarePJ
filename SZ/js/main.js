a446379d9e5b5ddee4be32d7e302dc1b6f19d1df
zjx292543239 <2925473239@qq.com>
2019-10-13 23:35:12 +0800

    Create main.js

---
+++ SZ/js/main.js
@@ -0,0 +1,20 @@
+var storage=window.localStorage;//获取当前locastorage中值
+function openweb(){
+    window.open('stu_info.html');
+    // if (storage.identity.value=='1')
+    //    window.open('su_info.html');
+    // else
+    //    window.open('tea_info.html');
+}
+
+function opensuanshi(btn1,btn2){
+    //将localstorage中保存年级和单元
+    //alert(btn);
+    var grade=btn1;
+    storage["grade"]=grade;
+    //alert(storage.grade);
+    unit=btn2;
+    storage['unit']=unit;
+    //alert(storage.unit);
+    window.open('suanshi.html');
+}
